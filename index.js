c26e042384098c677356a92af424c4779ac29c6b
N-o-o-b-Dev <140104064+N-o-o-b-Dev@users.noreply.github.com>
2024-11-22 16:08:28 +0530

    Day 33

---
+++ index.js
@@ -0,0 +1,8 @@
+var state ={
+    taskList:[]
+}
+
+var taskContent = document.querySelector("task_contents");
+var taskModal = document.querySelector("task-modal-body")
+
+console.log(taskContent+" \n"+taskModal)
